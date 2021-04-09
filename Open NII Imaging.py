5cfeea9363d276d74c4347a995437fc35270ab31
cw-raspdev <79803156+cw-raspdev@users.noreply.github.com>
2021-04-08 21:59:20 -0400

    Better logic flow for 3D arrays as opposed to 4D ones; explored nilearn which was a struggle

--- Open NII Imaging.py
+++ Open NII Imaging.py
@@ -2,13 +2,12 @@ import nibabel as nib
 import matplotlib.pyplot as plt
 
 # Change the file path to your path
-path = 'TestData/sub-01 copy/func/sub-01_task-Digit_run-01_bold.nii'
+path = 'TestData/sub-01 copy/anat/sub-01_T1w.nii.gz'
 my_img = nib.load(path)
 nii_data = my_img.get_fdata()
 nii_aff = my_img.affine
 nii_hdr = my_img.header
 sections = nii_data.shape[2]  # this is how many cross-sections there are
-time = nii_data.shape[3] # this is how many seconds(?) passed while subject was doing the task
 print(nii_aff, '\n', nii_hdr)
 print(nii_data.shape)
 
@@ -19,22 +18,35 @@ viewtype = input('--------\n'
                  'Enter Input Here:')
 if viewtype == '1':
     usersection1 = input('Which cross section? Please input an integer:\n')
-    usertime1 = input('What is initial time in range? Please input an integer:\n')
-    usertime2 = input('What is final time in range? Please input an integer:\n')
-    usersection1, usertime1, usertime2 = int(usersection1), int(usertime1), int(usertime2)
-    print('Thank you. Now analyzing NIFTI file. Beep boop beep')
-    for frame in range(usertime1, usertime2):
-        plt.imshow(nii_data[:, :, usersection1, frame])
+    usersection1 = int(usersection1)
+    if len(nii_data.shape) == 3:
+        print('Thank you. Now analyzing NIFTI file. Beep boop beep')
+        plt.imshow(nii_data[:, :, usersection1])
         plt.show()
+    elif len(nii_data.shape) == 4:
+        usertime1 = input('What is initial time in range? Please input an integer:\n')
+        usertime2 = input('What is final time in range? Please input an integer:\n')
+        usertime1, usertime2 = int(usertime1), int(usertime2)
+        print('Thank you. Now analyzing NIFTI file. Beep boop beep')
+        for frame in range(usertime1, usertime2):
+            plt.imshow(nii_data[:, :, usersection1, frame])
+            plt.show()
 elif viewtype == '2':
-    usertime1 = input('Which point in time? Please input an integer:\n')
     usersection1 = input('What is initial section in range? Please input an integer:\n')
     usersection2 = input('What is final section in range? Please input an integer:\n')
-    usertime1, usersection1, usersection2 = int(usertime1), int(usersection1), int(usersection2)
-    print('Thank you. Now analyzing NIFTI file. Boop beep boop')
-    for frame in range(usersection1, usersection2):
-        plt.imshow(nii_data[:, :, frame, usertime1])
-        plt.show()
+    usersection1, usersection2 = int(usersection1), int(usersection2)
+    if len(nii_data.shape) == 3:
+        print('Thank you. Now analyzing NIFTI file. Boop beep boop')
+        for frame in range(usersection1, usersection2):
+            plt.imshow(nii_data[:, :, frame])
+            plt.show()
+    elif len(nii_data.shape) == 4:
+        usertime1 = input('Which point in time? Please input an integer:\n')
+        usertime1 = int(usertime1)
+        print('Thank you. Now analyzing NIFTI file. Boop beep boop')
+        for frame in range(usersection1, usersection2):
+            plt.imshow(nii_data[:, :, frame, usertime1])
+            plt.show()
 else:
     print('Sorry, your input was not recognized. Please try again.')
 
