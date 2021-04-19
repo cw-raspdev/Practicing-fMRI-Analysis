fdebc125157ad8ed48e14b6f14b4f4c02546ce9d
cw-raspdev <79803156+cw-raspdev@users.noreply.github.com>
2021-04-19 19:09:10 -0400

    Should work now! :D

--- z-scoring.py
+++ z-scoring.py
@@ -25,6 +25,7 @@ from sklearn import preprocessing
 #this will normalize the response within voxels over time
 scaler = preprocessing.StandardScaler().fit(maskedData) #returns as a fitted model
 maskedData_zscore = scaler.transform(maskedData) #returns the transformed data
+maskedData_zscore = maskedData_zscore.tolist()
 
 #check the z-scoring by printing out the mean and standard deviation of individual voxels
 voxel_mean = np.mean(maskedData_zscore, axis=0)
@@ -34,11 +35,11 @@ print('The mean of the first few voxels:\n', voxel_mean[0:4])
 print('The std of the first few voxels:\n', voxel_std[0:4])
 
 #plot the distribution of values for a z-scored voxel as a histogram
-n, bins, patches = plt.hist(x=maskedData_zscore, bins='auto',
-                            alpha=0.75, rwidth=0.85)
+plt.hist(voxel_mean[0:500], bins='auto', alpha=0.75, rwidth=2)
 plt.grid(axis='y', alpha=0.75)
 plt.xlabel('Voxel Means')
 plt.ylabel('Frequency')
 plt.title('Z-scored Data for Voxels')
 plt.text(23, 45, r'$\mu=15, b=3$')
-maxfreq = n.max()
+plt.show()
+
