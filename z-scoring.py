28da2df917c5774ad00aafb90f0885c0d854bcd2
mehakmalhotra <mehak_malhotra@brown.edu>
2021-04-19 18:21:29 -0400

    Voxel-time plots

--- z-scoring.py
+++ z-scoring.py
@@ -34,8 +34,8 @@ print('The mean of the first few voxels:\n', voxel_mean[0:4])
 print('The std of the first few voxels:\n', voxel_std[0:4])
 
 #plot the distribution of values for a z-scored voxel as a histogram
-n, bins, patches = plt.hist(x=maskedData_zscore, bins='auto', color='#0504aa',
-                            alpha=0.7, rwidth=0.85)
+n, bins, patches = plt.hist(x=maskedData_zscore, bins='auto',
+                            alpha=0.75, rwidth=0.85)
 plt.grid(axis='y', alpha=0.75)
 plt.xlabel('Voxel Means')
 plt.ylabel('Frequency')
