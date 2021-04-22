b9d3aa314b883d69c690c5e6134e6235f6a10c21
cw-raspdev <79803156+cw-raspdev@users.noreply.github.com>
2021-04-22 00:44:03 -0400

    Improved prompts

--- Open NII Imaging.py
+++ Open NII Imaging.py
@@ -8,7 +8,7 @@ nii_data = my_img.get_fdata()
 nii_aff = my_img.affine
 nii_hdr = my_img.header
 sections = nii_data.shape[2]  # this is how many cross-sections there are
-# print(nii_aff, '\n', nii_hdr)
+
 print(nii_data.shape)
 
 viewtype = input('--------\n'
@@ -17,23 +17,23 @@ viewtype = input('--------\n'
                  ' 2) View a specific range of cross sections for one point in time\n'
                  'Enter Input Here:')
 if viewtype == '1':
-    usersection1 = input('Which cross section? Please input an integer:\n')
+    usersection1 = input('Which cross section? Please input an integer between 0 and '+str(nii_data.shape[2]-1)+':\n')
     usersection1 = int(usersection1)
     if len(nii_data.shape) == 3:
         print('Thank you. Now analyzing NIFTI file. Beep boop beep')
         plt.imshow(nii_data[:, :, usersection1])
         plt.show()
     elif len(nii_data.shape) == 4:
-        usertime1 = input('What is initial time in range? Please input an integer:\n')
-        usertime2 = input('What is final time in range? Please input an integer:\n')
+        usertime1 = input('What is initial time in range? Please input an integer between 0 and '+str(nii_data.shape[3]-1)+':\n')
+        usertime2 = input('What is final time in range? Please input an integer between 0 and '+str(nii_data.shape[3]-1)+':\n')
         usertime1, usertime2 = int(usertime1), int(usertime2)
         print('Thank you. Now analyzing NIFTI file. Beep boop beep')
         for frame in range(usertime1, usertime2):
             plt.imshow(nii_data[:, :, usersection1, frame])
             plt.show()
 elif viewtype == '2':
-    usersection1 = input('What is initial section in range? Please input an integer:\n')
-    usersection2 = input('What is final section in range? Please input an integer:\n')
+    usersection1 = input('What is initial section in range? Please input an integer between 0 and '+str(nii_data.shape[2]-1)+':\n')
+    usersection2 = input('What is final section in range? Please input an integer between 0 and '+str(nii_data.shape[2]-1)+':\n')
     usersection1, usersection2 = int(usersection1), int(usersection2)
     if len(nii_data.shape) == 3:
         print('Thank you. Now analyzing NIFTI file. Boop beep boop')
@@ -49,30 +49,3 @@ elif viewtype == '2':
             plt.show()
 else:
     print('Sorry, your input was not recognized. Please try again.')
-
-# if userdata == 'all':
-# #   if len(nii_data.shape) == 3:
-# #       for slice_Number in range(nii_data.shape[2]):
-# #           plt.imshow(nii_data[:, :, slice_Number])
-# #           plt.show()
-#     if len(nii_data.shape) == 4:
-#         for frame in range(nii_data.shape[3]):
-#             for slice_Number in range(nii_data.shape[2]):
-#                 if sections > 0:
-#                     plt.imshow(nii_data[:, :, slice_Number, frame])
-#                     plt.show()
-#                     sections = sections - 1
-# else:
-#     userdata = int(userdata)
-# #   if len(nii_data.shape) == 3:
-# #       for slice_Number in range(nii_data.shape[2]):
-# #           plt.imshow(nii_data[:, :, slice_Number])
-# #           plt.show()
-#     if len(nii_data.shape) == 4:
-#         for frame in range(nii_data.shape[3]):
-#             plt.imshow(nii_data[:, :, userdata, frame])
-#             plt.show()
-#             sections = sections - 1
-
-
-
